3380fe17a4ad54f4c0669bc20baab91b48b5027c
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-05-27 20:18:02 +0530

    feat: Login Added

---
+++ lib/screens/loginsrc.dart
@@ -0,0 +1,267 @@
+import 'package:insighteye_web/constants/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../wrapper/homewrapper.dart';
+
+class LoginSrc extends StatefulWidget {
+  const LoginSrc({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginSrcState createState() => _LoginSrcState();
+}
+
+class _LoginSrcState extends State<LoginSrc> {
+  final _formKey = GlobalKey<FormState>();
+
+  // editing controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final _auth = FirebaseAuth.instance;
+
+  // string for displaying the error Message
+  String? errorMessage;
+  bool load = false;
+
+  @override
+  Widget build(BuildContext context) {
+    //email field
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Please Enter Your Email");
+          }
+          // reg expression for email validation
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            return ("Please Enter a valid email");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          emailController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.mail),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //password field
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      validator: (value) {
+        RegExp regex = RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Password(Min. 6 Character)");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.vpn_key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onFieldSubmitted: (value) =>
+          signIn(emailController.text, passwordController.text),
+    );
+
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: TextButton(
+          onPressed: () {
+            signIn(emailController.text, passwordController.text);
+          },
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            child: Text(
+              "Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          )),
+    );
+
+    return Scaffold(
+      backgroundColor: newbg,
+      body: Stack(children: [
+        Center(
+          child: SingleChildScrollView(
+            child: Container(
+              color: newbg,
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white),
+                  width: MediaQuery.of(context).size.width / 3,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                            height: 100,
+                            width: 150,
+                            child: Image.asset(
+                              "assets/icons/appicon.png",
+                              fit: BoxFit.contain,
+                              filterQuality: FilterQuality.high,
+                            )),
+                        const SizedBox(height: 45),
+                        emailField,
+                        const SizedBox(height: 25),
+                        passwordField,
+                        const SizedBox(height: 35),
+                        loginButton,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: load
+              ? const CircularProgressIndicator(
+                  color: Colors.redAccent,
+                )
+              : null,
+        )
+      ]),
+    );
+  }
+
+  // login function
+  void signIn(String email, String password) async {
+    // firebase
+    // if (_formKey.currentState!.validate()) {
+    setState(() {
+      load = true;
+    });
+    try {
+      await _auth
+          .signInWithEmailAndPassword(
+              email: "bhavya@gmail.com", password: "12345678")
+          // .signInWithEmailAndPassword(email: email, password: password)
+          .then((uid) => {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const HomeWrapper())),
+              });
+    } catch (error) {
+      setState(() {
+        load = false;
+      });
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const SimpleCustomAlert("Email or Password is incorrect!");
+          });
+    }
+    // }
+  }
+}
+
+class SimpleCustomAlert extends StatelessWidget {
+  final String title;
+  const SimpleCustomAlert(this.title, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.redAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: SizedBox(
+        height: 200,
+        width: 450,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.warning_amber_rounded,
+                    color: primarybg,
+                    size: 30,
+                  ),
+                  Text(
+                    " Error",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Incorrect username or password",
+              style: TextStyle(
+                fontFamily: "Nunito",
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 28,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(white),
+              ),
+              child: const Text(
+                "Okay",
+                style: TextStyle(
+                  fontFamily: "Nunito",
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
